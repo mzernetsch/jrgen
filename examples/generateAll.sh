72904480d46378a6070b46d5e9bb829c2911bf01
Marco Zernetsch <m.zernetsch@gmail.com>
2017-11-20 21:49:18 +0100

    chore: make examples script use relative paths

--- examples/generateAll.sh
+++ examples/generateAll.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-schema=${1:-"./examples/ExampleAPI.schema.json"}
+SCRIPT_PATH="`dirname \"$0\"`"
+JRGEN_PATH=$SCRIPT_PATH/../jrgen.js
+SCHEMA_PATH=${1:-$SCRIPT_PATH"/ExampleAPI.schema.json"}
 
 #docs
-node ./jrgen.js docs -f html -o ./examples/docs/html $schema
-node ./jrgen.js docs -f md -o ./examples/docs/md $schema
-node ./jrgen.js docs -f gitbook -o ./examples/docs/gitbook $schema
+node $JRGEN_PATH docs -f html -o $SCRIPT_PATH/docs/html $SCHEMA_PATH
+node $JRGEN_PATH docs -f md -o $SCRIPT_PATH/docs/md $SCHEMA_PATH
+node $JRGEN_PATH docs -f gitbook -o $SCRIPT_PATH/docs/gitbook $SCHEMA_PATH
 
 #test
-node ./jrgen.js test -f jasmine -o ./examples/test/jasmine $schema
+node $JRGEN_PATH test -f jasmine -o $SCRIPT_PATH/test/jasmine $SCHEMA_PATH
 
 #client
-node ./jrgen.js client -f es6 -o ./examples/client/es6 $schema
-node ./jrgen.js client -f ts -o ./examples/client/ts $schema
+node $JRGEN_PATH client -f es6 -o $SCRIPT_PATH/client/es6 $SCHEMA_PATH
+node $JRGEN_PATH client -f ts -o $SCRIPT_PATH/client/ts $SCHEMA_PATH
 
 #server
-node ./jrgen.js server -f nodejs -o ./examples/server/nodejs $schema
+node $JRGEN_PATH server -f nodejs -o $SCRIPT_PATH/server/nodejs $SCHEMA_PATH
