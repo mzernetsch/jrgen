33d6325d6a39c97c51d9d00ae037b3d5cb931c3a
Authur Lan <authurlan@gmail.com>
2022-01-25 14:42:08 +0800

    feat: support YAML

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const fs = require("fs-extra");
 const path = require("path");
 const $RefParser = require("json-schema-ref-parser");
-const jsonlint = require("jsonlint");
+const yaml = require("js-yaml");
 const prettier = require("prettier");
 const glob = require("glob");
 const mustache = require("mustache");
@@ -214,7 +214,7 @@ exports.loadSchema = (schemaPath) => {
     encoding: "utf8",
   });
 
-  const schema = jsonlint.parse(jsonString);
+  const schema = yaml.load(jsonString);
 
   const resolvedSchema = exports.resolveSchemaRefs(schema);
 
